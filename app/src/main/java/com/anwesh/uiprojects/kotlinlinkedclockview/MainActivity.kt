20f8a4fb035293ec7cbef2b6a31c5a6e55f08201
Anwesh43 <anweshthecool0@gmail.com>
2018-06-05 11:14:20 +0530

    adding LinkedClockView in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkedclockview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkedclockview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.kotlinlinkedclockview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.linkedclockview.LinkedClockView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        LinkedClockView.create(this)
     }
 }
